f8ec370c5567589fc3b11fa48ec8931e5d2e4768
Juha-Pekka Tulijoki <jp.tulijoki@gmail.com>
2020-06-17 09:16:46 +0300

    suorituskykytestit: 50 siirtoa + pelilaudan arviointityökalu

--- src/main/java/datastructureproject/PerformanceTest.java
+++ src/main/java/datastructureproject/PerformanceTest.java
@@ -9,23 +9,51 @@ import chess.model.Side;
 public class PerformanceTest {
 
     public static void main(String[] args) {
-        test50moves(0);
+        testEvaluationTools();
+        test50moves(1);
+        test50moves(2);
+        test50moves(3);
     }
+    
+    public static void testEvaluationTools() {
+        Game game = new Game();
+        game.initBoard();
+        MoveSelector ms = new MoveSelector(game, 2, false, 0.0, false, 0.0);
+        Piece[][] board = null;
+        long time = 0;
+        for (int i = 1; i <= 10; i++) {  
+            board = ms.getBestWhiteMove();
+            game.setCurrentBoard(board);
+            long start = System.nanoTime();
+            ms.evaluateBoard(board);
+            long end = System.nanoTime();
+            time += end - start;
+            board = ms.getBestBlackMove();
+            game.setCurrentBoard(board);
+            start = System.nanoTime();
+            ms.evaluateBoard(board);
+            end = System.nanoTime();
+            time += end - start;
+        }
+        System.out.println(time / 20 + " ns");
+        printBoard(board);
+    }
+    
     public static void test50moves(int depth) {     
         Game game = new Game();
-        MoveSelector ms = new MoveSelector(game, depth, false, 0.0, false, 0.0);
         game.initBoard();
+        MoveSelector ms = new MoveSelector(game, depth, false, 0.0, false, 0.0);
+        Piece[][] board = null;
         long start = System.nanoTime();
-        for (int i = 1; i <= 50; i++) {
-            Piece[][] board = game.getCurrentBoard();
+        for (int i = 1; i <= 50; i++) {  
             board = ms.getBestWhiteMove();
             game.setCurrentBoard(board);
             board = ms.getBestBlackMove();
             game.setCurrentBoard(board);
         }
         long end = System.nanoTime();
-        System.out.println((end - start) / 1e9 + " sec.");
-        Piece[][] board = game.getCurrentBoard();
+        System.out.println((end - start) / 1e9 + " sec");
+        board = game.getCurrentBoard();
         printBoard(board);
     }
     
